2e678990a0597540359418026be2ff3e53b46b83
Bian2017 <bgl_cumt_zju@163.com>
2019-09-10 09:27:45 +0800

    feat:添加sourcemap文件的配置

--- webpack.dev.js
+++ webpack.dev.js
@@ -185,5 +185,19 @@ module.exports = {
   devServer: {
     contentBase: './dist', // webpack-dev-server服务的基础目录
     hot: true
-  }
+  },
+  /**
+   * source-map
+   *
+   * 作用：通过source-map定位到源代码。通常开发环境开启，线上环境关闭。若线上
+   * 需排查问题，可以将sourcemap上传到错误监控系统。
+   *
+   * 关键字：
+   * + eval: 使用eval包裹模块代码；
+   * + source map: 产生.map文件；
+   * + cheap: 不包含列信息；
+   * + inline: 将.map作为DataURI嵌入，不单独生成.map文件(打包时会将sourcemap和js文件打包在一起)；
+   * + module: 包含loader的sourcemap；
+   */
+  devtool: 'source-map'
 }
